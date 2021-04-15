8f51a12eae631df0e0acca614a8e0973fadc2a55
Баранчик Татьяна <bar-ti@mail.ru>
2021-04-15 21:42:22 +0400

    сценарий регистрации пользователя

--- seleniumTests/src/test/java/CreateNewUserTest.java
+++ seleniumTests/src/test/java/CreateNewUserTest.java
@@ -1,5 +1,4 @@
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -9,8 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import static java.lang.Thread.sleep;
